f8274feb831b249ec14b715d04c766d8419fd6ea
Nikita Kirpichoev <nalkire17@gmail.com>
2022-03-29 08:16:03 +0300

    final

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'покушать', done: true },
+    { description: 'поспать', done: false },
+    { description: 'поиграть', done: false },
+    { description: 'прогулять пары', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+}
+
+
